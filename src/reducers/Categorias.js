9f73ce6c3e04622a47699424368d812f0f7e9df3
Joaquin Mellado Quevedo <mellado.joaquin@gmail.com>
2020-12-15 11:55:02 -0300

    spread operator to state.data

--- src/reducers/Categorias.js
+++ src/reducers/Categorias.js
@@ -1,1 +1,21 @@
 const ADD_CATEGORY = 'category/add'
+
+const addCatergory = payload => ({
+    type: ADD_CATEGORY,
+    payload
+})
+
+const initialState = {
+    data: [{ id: 1, name: 'Defecto' }]
+}
+export default function reducer(state = initialState, action) {
+    switch (action.type) {
+        case ADD_CATEGORY:
+            return {
+                ...state,
+                data: [...state.data, action.payload]
+            }
+        default:
+            break;
+    }
+}
