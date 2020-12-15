fa38680369b7d2911314aacacbdccec554c64a70
Joaquin Mellado Quevedo <mellado.joaquin@gmail.com>
2020-12-15 11:59:20 -0300

    returning state

--- src/reducers/Categorias.js
+++ src/reducers/Categorias.js
@@ -19,6 +19,6 @@ export default function reducer(state = initialState, action) {
                 data: [...state.data, action.payload]
             }
         default:
-            break;
+            return state
     }
 }
