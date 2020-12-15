384f2542e54717f35bf9fa8f9eaed7808b7f4b7f
Joaquin Mellado Quevedo <mellado.joaquin@gmail.com>
2020-12-15 11:25:31 -0300

    reducer was imported as formReducer alias

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import { Provider } from 'react-redux'
+import { reducer as formReducer } from 'redux-form'
 import { createStore, combineReducers } from 'redux'
 import './index.css'
 import App from './App'
