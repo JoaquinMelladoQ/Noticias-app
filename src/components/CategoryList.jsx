9e2cf386260c618f82729152de79c4b9c32e498f
Joaquin Mellado Quevedo <mellado.joaquin@gmail.com>
2020-12-15 13:49:58 -0300

    showing a category by default

--- src/components/CategoryList.jsx
+++ src/components/CategoryList.jsx
@@ -3,12 +3,9 @@ import React, { Component } from 'react'
 export default class CategoryList extends Component {
     render() {
         const { categories } = this.props
-        console.log(categories)
         return (
             <ul>
-                <li>Category 1</li>
-                <li>Category 2</li>
-                <li>Category 3 </li>
+                {categories.map(x => <li key={x.id}>{x.name}</li>)}
             </ul>
         )
     }
