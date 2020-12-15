552d6f611780a557e755a06ff240474bbc6be9fa
Joaquin Mellado Quevedo <mellado.joaquin@gmail.com>
2020-12-15 12:40:15 -0300

    category and news imported and render but not created yet

--- src/App.js
+++ src/App.js
@@ -2,11 +2,15 @@ import React, { Component } from 'react'
 import logo from './logo.svg'
 import './App.css'
 import { connect } from 'react-redux'
+import Category from './components/Category'
+import News from './components/News'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
+        <Category />
+        <News />
       </div>
     );
   }
