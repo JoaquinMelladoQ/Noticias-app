cb573cea8131387e12fb4dabbb8b559dbcbc2a58
Joaquin Mellado Quevedo <mellado.joaquin@gmail.com>
2020-12-16 22:37:14 -0300

    added a case to evaluate an action type SELECT_CATEGORY

--- src/reducers/Categorias.js
+++ src/reducers/Categorias.js
@@ -1,6 +1,7 @@
 const ADD_CATEGORY = 'category/add'
+const SELECT_CATEGORY = 'category/select'
 
-const addCatergory = payload => ({
+export const addCatergory = payload => ({
     type: ADD_CATEGORY,
     payload: {
         ...payload,
@@ -8,15 +9,26 @@ const addCatergory = payload => ({
     }
 })
 
+export const selectCategory = payload => ({
+    type: SELECT_CATEGORY,
+    payload, 
+})
+
 const initialState = {
-    data: [{ id: 1, name: 'Defecto' }]
+    data: [{ id: 1, name: 'Defecto' }],
+    selected: 1,
 }
-export default function reducer (state = initialState, action = addCatergory){
+export default function reducer (state = initialState, action){
     switch (action.type) {
         case ADD_CATEGORY:
             return {
                 ...state,
                 data: [...state.data, action.payload]
+            }
+        case SELECT_CATEGORY:
+            return {
+                ...state,
+                selected: action.payload
             }
         default:
             return state
